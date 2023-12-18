f1eb6b8c4bac6de6708d5aeede6c7f416e2ebd79
Devvy2 <jsorenesh04.kodehode@gmail.com>
2023-12-18 13:32:10 +0100

    Update Profile page UI

--- Client/src/Pages/profile/Profile.jsx
+++ Client/src/Pages/profile/Profile.jsx
@@ -1,9 +1,26 @@
+import { useSelector } from "react-redux";
+import "./Profile.css";
+
 function Profile() {
-    return (
-        <div>
-            <h1>Profile</h1>
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="Profile-page">
+      <div>
+        <h1>Profile</h1>
+        <form className="Profile-info">
+          <img className="Profile-img" src={currentUser.avatar} alt="" />
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button className="Update-btn">UPDATE</button>
+        </form>
+        <div className="account-actions">
+          <span>Delete account</span>
+          <span>Sign out</span>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
